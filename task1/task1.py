62ffbe2b55806da01b043a20c6b991c0c3757544
Pavel <125222765+PavelRedd@users.noreply.github.com>
2024-07-14 14:36:19 +0300

    upload task1

--- task1/task1.py
+++ task1/task1.py
@@ -1,1 +1,20 @@
+def main():
+    n = int(input("n = "))
+    m = int(input("m = "))
 
+    arr = list(range(1, n + 1))
+
+    current = 0
+    path = []
+
+    print("Path: ", end="")
+    while True:
+        print(arr[current], end="")
+        path.append(arr[current])
+        current = (current + m - 1) % n
+        if current == 0:
+            break
+    print()
+
+if __name__ == "__main__":
+    main()
