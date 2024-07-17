00eccb729da81224a785f9abe85dd0e144c6b213
Pavel <125222765+PavelRedd@users.noreply.github.com>
2024-07-18 00:31:01 +0300

    Update task1.py/console input

--- task1/task1.py
+++ task1/task1.py
@@ -1,19 +1,32 @@
+import sys
+
 def main():
-    n = int(input("n = "))
-    m = int(input("m = "))
+    if len(sys.argv) < 3:
+        n = int(input("n = "))
+        m = int(input("m = "))
+    else:
+        n = int(sys.argv[1])
+        m = int(sys.argv[2])
 
     arr = list(range(1, n + 1))
-
+    print("Массив: ", "".join(map(str, arr)))
     current = 0
     path = []
-
-    print("Path: ", end="")
+    intervals = []
+    print("Путь: ", end="")
     while True:
         print(arr[current], end="")
         path.append(arr[current])
+        interval = [arr[(current + i) % n] for i in range(m)]
+        intervals.append(interval)
+
         current = (current + m - 1) % n
         if current == 0:
             break
+    print()
+    print("Интервалы: ", end="")
+    for interval in intervals:
+        print("".join(map(str, interval)), end=" ")
     print()
 
 if __name__ == "__main__":
