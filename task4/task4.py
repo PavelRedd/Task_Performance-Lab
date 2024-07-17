78748c3db60d1cc7fb7d8a65810bf5816459f6ae
Pavel <125222765+PavelRedd@users.noreply.github.com>
2024-07-18 00:33:46 +0300

    Update task4.py/console input

--- task4/task4.py
+++ task4/task4.py
@@ -1,9 +1,17 @@
+import sys
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python task4.py <D:/.../path_to_file>")
+        return
+    path_nums = sys.argv[1]
+    with open(path_nums) as f:
+        nums = f.readlines()
+        numbers = [int(num.strip()) for num in nums]  
+    numbers.sort()
+    median = len(numbers) // 2
+    result = sum(abs(n - numbers[median]) for n in numbers)
+    print(result)
+
 if __name__ == '__main__':
-	path_nums = input('Путь до файла c числами: ')
-	with open(path_nums) as f:
-		nums = f.readlines()
-		numbers = [int(num) for num in nums]
-	numbers.sort()
-	median = len(numbers) // 2
-	result = 0
-	print(sum(abs(n - numbers[median]) for n in numbers))
+    main()
